4503735d4295834b27e7b4453c45e6317af6da12
adamtopaz <github@adamtopaz.com>
2024-11-01 11:14:10 -0600

    add some more scripts

--- Main.lean
+++ Main.lean
@@ -34,6 +34,17 @@ def runOnMathlibConsts
         try withTimeout t go
         catch _e => return .ok ()
 
+def writeExprGraph (handle : IO.FS.Handle) (expr : Expr) (mdata : Json) : MetaM Unit := do
+  if ← IO.checkCanceled then return
+  let (node, graph) ← expr.mkExprGraph true true
+  if ← IO.checkCanceled then return
+  handle.putStrLn <| Json.compress <| .mkObj [
+    ("mdata", mdata),
+    ("pp", toString <| ← Meta.ppExpr expr),
+    ("graph", graph.mkJsonWithIdx node (fun a => toJson a.val) (fun a => toJson a.val)),
+    ("dot", graph.mkDotWithIdx node (fun a => a.val.toString) (fun a => a.val.toString) hash)
+  ]
+
 def runTypeGraphCmd (p : Parsed) : IO UInt32 := do
   let output : String := p.positionalArg! "output" |>.as! String
   let threads : Nat := p.positionalArg! "threads" |>.as! Nat
@@ -45,15 +56,11 @@ def runTypeGraphCmd (p : Parsed) : IO UInt32 := do
       (numThread := threads) (opts := options) (timeout := timeout.toUInt32) 
       fun idx nm cinfo => Meta.MetaM.run' do
     let tp := cinfo.type
-    let (node, graph) ← tp.mkExprGraph true true
     let mod := (← getEnv).getModuleFor? nm
     println! s!"{idx} : {nm} : {mod.getD .anonymous}" 
-    handle.putStrLn <| Json.compress <| .mkObj [
+    writeExprGraph handle tp <| .mkObj [
       ("name", toJson nm),
-      ("module", toJson mod),
-      ("pp", toString <| ← Meta.ppExpr tp),
-      ("graph", graph.mkJsonWithIdx node (fun a => toJson a.val) (fun a => toJson a.val)),
-      ("dot", graph.mkDotWithIdx node (fun a => a.val.toString) (fun a => a.val.toString) hash)
+      ("module", toJson mod)
     ]
     return .ok ()
   match res with 
@@ -69,10 +76,92 @@ def typeGraphCmd := `[Cli|
     "timeout" : Nat; "Timeout for each calculation in milliseconds"
 ]
 
+def runTypeValGraphCmd (p : Parsed) : IO UInt32 := do
+  let output : String := p.positionalArg! "output" |>.as! String
+  let threads : Nat := p.positionalArg! "threads" |>.as! Nat
+  let timeout : Nat := p.positionalArg! "timeout" |>.as! Nat
+  let output : System.FilePath := output
+  let handle ← IO.FS.Handle.mk output .write
+  let options : Options := maxHeartbeats.set {} 0
+  let res ← runOnMathlibConsts 
+      (numThread := threads) (opts := options) (timeout := timeout.toUInt32) 
+      fun idx nm cinfo => Meta.MetaM.run' do
+    let tp := cinfo.type
+    let mod := (← getEnv).getModuleFor? nm
+    println! s!"{idx} : {nm} : {mod.getD .anonymous}" 
+    writeExprGraph handle tp <| .mkObj [
+      ("kind", "type"),
+      ("name", toJson nm),
+      ("module", toJson mod)
+    ]
+    let some val := cinfo.value? | return .ok ()
+    writeExprGraph handle val <| .mkObj [
+      ("kind", "value"),
+      ("name", toJson nm),
+      ("module", toJson mod)
+    ]
+    return .ok ()
+  match res with 
+  | .ok _ => return 0
+  | .error e => throw e
+
+def typeValGraphCmd := `[Cli|
+  type_val_graph VIA runTypeValGraphCmd;
+  "Generate graphs for types and values of constants appearing in mathlib."
+  ARGS:
+    "output" : String; "Output file"
+    "threads" : Nat; "Number of threads to use"
+    "timeout" : Nat; "Timeout for each calculation in milliseconds"
+]
+
+def runSubexprGraphCmd (p : Parsed) : IO UInt32 := do
+  let output : String := p.positionalArg! "output" |>.as! String
+  let threads : Nat := p.positionalArg! "threads" |>.as! Nat
+  let timeout : Nat := p.positionalArg! "timeout" |>.as! Nat
+  let output : System.FilePath := output
+  let handle ← IO.FS.Handle.mk output .write
+  let options : Options := maxHeartbeats.set {} 0
+  let res ← runOnMathlibConsts 
+      (numThread := threads) (opts := options) (timeout := timeout.toUInt32) 
+      fun idx nm cinfo => Meta.MetaM.run' do
+    let tp := cinfo.type
+    let mod := (← getEnv).getModuleFor? nm
+    println! s!"{idx} : {nm} : {mod.getD .anonymous}" 
+    Meta.forEachExpr tp fun e => do
+      writeExprGraph handle e <| .mkObj [
+        ("kind", "type"),
+        ("name", toJson nm),
+        ("module", toJson mod)
+      ]
+    let some val := cinfo.value? | return .ok ()
+    Meta.forEachExpr val fun e => do
+      writeExprGraph handle e <| .mkObj [
+        ("kind", "value"),
+        ("name", toJson nm),
+        ("module", toJson mod)
+      ]
+    return .ok ()
+  match res with 
+  | .ok _ => return 0
+  | .error e => throw e
+
+def subexprGraphCmd := `[Cli|
+  subexpr_graph VIA runTypeValGraphCmd;
+  "Generate graphs for all (sub)expressions appearing in mathlib."
+  ARGS:
+    "output" : String; "Output file"
+    "threads" : Nat; "Number of threads to use"
+    "timeout" : Nat; "Timeout for each calculation in milliseconds"
+]
+
 def entrypoint := `[Cli|
   entrypoint NOOP; "Entry point for this program"
   SUBCOMMANDS:
-  typeGraphCmd
+  typeGraphCmd;
+  typeValGraphCmd;
+  subexprGraphCmd
 ]
+
+
 
 def main (args : List String) : IO UInt32 := entrypoint.validate args
